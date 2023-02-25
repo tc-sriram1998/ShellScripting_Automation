24087082bfe76d5810bf3666402a7bf313e00877
tc-sriram1998 <119699151+tc-sriram1998@users.noreply.github.com>
2023-02-25 17:06:24 +0530

    Update automation5.sh

--- automation5.sh
+++ automation5.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 ONE=14d
 
-Fourteendaysold=`date -v -${ONE}`
+Fourteendaysold=$(date -d "$date -14 days" +"%Y%m%d")
 #Fourteendaysold=`date -d "$dataset_date - $ONE days" +%F`
 echo $Fourteendaysold
 
 TWO=7d
-sevendaysold=`date -v -${TWO}`
+sevendaysold=$(date -d "$date -7 days" +"%Y%m%d")
 #sevendaysold=`date -d "$dataset_date - $TWO days" +%F`
 echo $sevendaysold
 #date1=`echo $sevendaysold | sed 's/-//g’`
